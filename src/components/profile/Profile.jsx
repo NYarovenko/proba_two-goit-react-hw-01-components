178b46fd2e17da7e4d9362212163593978ed89dc
NYarovenko <NYarovenko.Full.Stack@gmail.com>
2024-02-11 23:16:56 +0200

    profile

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,29 @@
+import css from './Profile.module.css';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt={username} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">1000</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">2000</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">3000</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
